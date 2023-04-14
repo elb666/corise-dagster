17790683c91be813f86035cde02ba385c0db1a00
Eric Brown <ebrown22@bu.edu>
2023-04-14 12:36:16 -0400

    do the ops

--- week_2/workspaces/project/week_2.py
+++ week_2/workspaces/project/week_2.py
@@ -16,24 +16,51 @@ from workspaces.resources import mock_s3_resource, redis_resource, s3_resource
 from workspaces.types import Aggregation, Stock
 
 
-@op
-def get_s3_data():
-    pass
+@op(
+    config_schema={"s3_key": str},
+    out={"stocks": Out(dagster_type=List[Stock])},
+    required_resource_keys={"s3"}
+)
+def get_s3_data(context: OpExecutionContext):
+    s3_key = context.op_config["s3_key"]
+    stocks = []
+    for record in context.resources.s3.get_data(s3_key):
+        stocks.append(Stock.from_list(record))
+
+    return stocks
 
 
-@op
-def process_data():
-    pass
+@op(
+    description="You can also pass the description of the op in the as a parameter to the op decorator, like this",
+    ins={ "stocks": In(dagster_type = List[Stock], description="description of stocks input")},
+    out={ "high":  Out(dagster_type = Aggregation, description="the date and value of the high")},
+)
+def process_data(stocks):
+    highest = max(stocks, key=lambda k: k.high)
+    high = Aggregation(date=highest.date, high=highest.high)
+    return high
 
 
-@op
-def put_redis_data():
-    pass
+@op(
+    ins={ "aggregation": In(dagster_type = Aggregation, description="This is the aggregation that is written to redis")},
+    required_resource_keys={"redis"},
+)
+def put_redis_data(context: OpExecutionContext, aggregation: Aggregation):
+    name = aggregation.date.isoformat()
+    value = str(aggregation.high)
+
+    context.resources.redis.put_data(name=name, value=value)
 
 
-@op
-def put_s3_data():
-    pass
+@op(
+    ins={ "aggregation": In(dagster_type = Aggregation, description="This is the aggregation that is written to redis")},
+    required_resource_keys={"s3"},
+)
+def put_s3_data(context: OpExecutionContext, aggregation: Aggregation):
+    key_name = aggregation.date.isoformat()
+    data = str(aggregation.high)
+
+    context.resources.s3.put_data(key_name=key_name, data=data)
 
 
 @graph
